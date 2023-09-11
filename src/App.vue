00940302e8875a528f8fc8b32f46b53c98ca4220
Deependra <46601191+msatech@users.noreply.github.com>
2023-09-11 12:49:11 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="app" class="p-10">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
